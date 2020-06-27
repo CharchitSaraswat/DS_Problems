519f19e021d7730fcb5a1c7815ca29d5abd91d51
CharchitSaraswat <charchit91993@gmail.com>
2020-06-28 04:58:35 +0530

    editing distance: successful

--- Algorithm Toolbox UCSD /tdhq62PPSCuYautjz_grzQ_de94f9532f004a819a9ae54a66b29f25_course1_2005062005/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ Algorithm Toolbox UCSD /tdhq62PPSCuYautjz_grzQ_de94f9532f004a819a9ae54a66b29f25_course1_2005062005/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,23 @@
 # Uses python3
-def edit_distance(s, t):
+def edit_distance(a, b):
     #write your code here
-    return 0
+    c = list()
+    for i in range(len(a)+1):
+        ci = list()
+        for j in range(len(b)+1):
+            ci.append(0)
+        c.append(ci)
+    for i in range(len(a)+1):
+        for j in range(len(b)+1):
+            if i == 0:
+                c[i][j] = j
+            elif j == 0:
+                c[i][j] = i
+            elif a[i-1] == b[j-1]:
+                c[i][j] = c[i-1][j-1]
+            else:
+                c[i][j] = 1 + min(c[i][j-1], c[i-1][j], c[i-1][j-1])
+    return c[len(a)][len(b)]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
