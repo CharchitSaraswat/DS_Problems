b5424800a2950ecd40c400836f41c6ae74f9e33e
CharchitSaraswat <charchit91993@gmail.com>
2020-06-28 19:59:07 +0530

    lcs 3

--- Algorithm Toolbox UCSD /tdhq62PPSCuYautjz_grzQ_de94f9532f004a819a9ae54a66b29f25_course1_2005062005/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ Algorithm Toolbox UCSD /tdhq62PPSCuYautjz_grzQ_de94f9532f004a819a9ae54a66b29f25_course1_2005062005/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -4,7 +4,17 @@ import sys
 
 def lcs3(a, b, c):
     #write your code here
-    return min(len(a), len(b), len(c))
+    d = list()
+    d = [[[0 for k in range(cn+1)]
+                  for j in range(bn+1)] for i in range(an+1)]
+    for i in range(1, an+1):
+        for j in range(1, bn+1):
+            for k in range(1, cn+1):
+                if a[i-1] == b[j-1] and b[j-1] == c[k-1]:
+                    d[i][j][k] = d[i-1][j-1][k-1] + 1
+                else:
+                    d[i][j][k] = max(d[i-1][j][k], d[i][j-1][k], d[i][j][k-1])
+    return d[an][bn][cn]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
